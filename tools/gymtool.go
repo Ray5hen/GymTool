c276748979d18182d9efcb36f8b8c21de4cda6ab
ray5hen <limboray1@gmail.com>
2017-07-29 18:01:06 +0800

    add food

--- tools/gymtool.go
+++ tools/gymtool.go
@@ -11,48 +11,17 @@ func Gt(message string) string{
     switch toolCode {
     case string('k'),string('p') :
     response=weight(toolCode, message)
-
+    case string('f') :
+    response=food(message)
 
     }
+
+
+
 	return response
 
 }
-
-// func weight(x string) string{
-//     st :=x
-//     s := string(st[0])
-//     var aaa string
-    // if _, err := strconv.Atoi(st[1:len(st)]); err == nil {
-    //     value, err := strconv.ParseFloat(st[1:len(st)], 64)
-    //     if err != nil {
-    //     // do something sensible
-    //     }
-    //     amount := float64(value)
-    //     switch s {
-    //     case string('k') :
-    //     aaa=weightLbs(amount)+" Lbs"
-    //     //return weightLbs(amount)+" Lbs"
-    //     //fmt.Println(weightLbs(amount)+" Lbs")
-    //     case string('p'):
-    //     aaa=weightKg(amount)+" Kg"
-    //     //return weightKg(amount)+" Kg"
-    //     //fmt.Println(weightKg(amount)+" Kg")
-    //     }
-    // }else{
-    //     aaa="invalid input"
-    // }
-//     return aaa
-// }
-
-
-// func weightLbs(x string) string{
-
-//     return strconv.FormatFloat(x * 2.2, 'f', 2, 64)
-// }
-// func weightKg(x string) string{
-//     return strconv.FormatFloat(x * 0.45, 'f', 2, 64)
-// }
-
+    
 func weight(code string, msg string) string{
 
     if _, err := strconv.Atoi(msg[1:len(msg)]); err == nil {
@@ -62,10 +31,32 @@ func weight(code string, msg string) string{
         }
         amount := float64(value)
         if code=="k"{
-        return strconv.FormatFloat(amount * 0.45, 'f', 2, 64)
+        return strconv.FormatFloat(amount * 0.45, 'f', 2, 64) + "Kg"
         }else{
-        return strconv.FormatFloat(amount * 2.2, 'f', 2, 64)
+        return strconv.FormatFloat(amount * 2.2, 'f', 2, 64) + "Lbs"
         }
     }
     return "invalid input"
+}
+
+func food(msg string) string{
+    foods := map[string]string{
+    "雞蛋":"碳水(g): 0 蛋白質(g): 6",
+    "鮭魚":"碳水(g): 0 蛋白質(g): 20",
+    "雞胸":"碳水(g): 0 蛋白質(g): 22",
+    "牛奶":"碳水(g): 4 蛋白質(g): 3",
+    "鯛魚":"碳水(g): 2.5 蛋白質(g): 18",
+    "地瓜":"碳水(g): 25 蛋白質(g): 1.3",
+    "藜麥":"碳水(g): 68 蛋白質(g): 12",
+    "奇亞籽":"碳水(g): 44 蛋白質(g): 16",
+    "糙米":"碳水(g): 73 蛋白質(g): 9",
+    "鮪魚罐頭":"碳水(g):  蛋白質(g): 30",
+    "藍莓":"碳水(g): 13 蛋白質(g): 0",
+    "燕麥":"碳水(g): 76 蛋白質(g): 0",
+    }
+    var input = msg[1:len(msg)]
+    if f, ok := foods[input]; ok {
+    return f
+    }
+    return "No this food."
 }

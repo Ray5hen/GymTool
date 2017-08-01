98b3db8b02a678fcf19aa3406f9f063b1f4f1d28
ray5hen <limboray1@gmail.com>
2017-08-01 23:09:11 +0800

    modified food for read data from csv

--- tools/gymtool.go
+++ tools/gymtool.go
@@ -4,6 +4,10 @@ import (
 	"strconv"
     "strings"
     "math"
+    "encoding/csv"
+    "fmt"
+    "io"
+    "os"
 )
 
 func Gt(message string) string{
@@ -20,7 +24,7 @@ func Gt(message string) string{
     case string('b') :
     response=bodyinfo(message)
     case string('h') :
-    response="重量轉換:p數字(轉公斤),k數字(轉磅)\n長度轉換:m數字(轉吋與英尺),i數字(轉公尺)\n食物成份:f食物名稱\n1rm計算: r重量-最多可做組數(r100-8)"
+    response="重量轉換: p數字(轉公斤),k數字(轉磅)\n長度轉換: m數字(轉吋與英尺),i數字(轉公尺)\n食物成份: f食物名稱\n1rm計算: r重量-最多可做組數(r100-8)\n基礎代謝率(BMR): b-身高(公分)-體重(公斤)-年齡(整數)-性別(1男0女)"
     }
 	return response
 
@@ -48,39 +52,67 @@ func weight(code string, msg string) string{
 }
 
 func food(msg string) string{
-    //response :="No this food." 
-    var response string
-    other:=""
+    ///response :="No this food." 
+     //var response string
+    // other:=""
+     response:="test"
+    // foods := map[string]string{
+    // "雞蛋":"碳水化合物(g): 0 ,蛋白質(g): 6",
+    // "鮭魚":"碳水化合物(g): 0 ,蛋白質(g): 20",
+    // "雞胸":"碳水化合物(g): 0 ,蛋白質(g): 22",
+    // "牛奶":"碳水化合物(g): 4 ,蛋白質(g): 3",
+    // "鯛魚":"碳水化合物(g): 2.5 ,蛋白質(g): 18",
+    // "地瓜":"碳水化合物(g): 25 ,蛋白質(g): 1.3",
+    // "藜麥":"碳水化合物(g): 68 ,蛋白質(g): 12",
+    // "奇亞籽":"碳水化合物(g): 44 ,蛋白質(g): 16",
+    // "糙米":"碳水化合物(g): 73 ,蛋白質(g): 9",
+    // "鮪魚罐頭":"碳水化合物(g):  ,蛋白質(g): 30",
+    // "藍莓":"碳水化合物(g): 13 ,蛋白質(g): 0",
+    // "燕麥":"碳水化合物(g): 76 ,蛋白質(g): 0",
+    // }
+     var input = msg[1:len(msg)]
+    // if f, ok := foods[input]; ok {
+    // response= "每100克 "+input+" 含: "+f
+    // }else{
+    // response="找不到: "+input
+    // }
 
-    foods := map[string]string{
-    "雞蛋":"碳水化合物(g): 0 ,蛋白質(g): 6",
-    "鮭魚":"碳水化合物(g): 0 ,蛋白質(g): 20",
-    "雞胸":"碳水化合物(g): 0 ,蛋白質(g): 22",
-    "牛奶":"碳水化合物(g): 4 ,蛋白質(g): 3",
-    "鯛魚":"碳水化合物(g): 2.5 ,蛋白質(g): 18",
-    "地瓜":"碳水化合物(g): 25 ,蛋白質(g): 1.3",
-    "藜麥":"碳水化合物(g): 68 ,蛋白質(g): 12",
-    "奇亞籽":"碳水化合物(g): 44 ,蛋白質(g): 16",
-    "糙米":"碳水化合物(g): 73 ,蛋白質(g): 9",
-    "鮪魚罐頭":"碳水化合物(g):  ,蛋白質(g): 30",
-    "藍莓":"碳水化合物(g): 13 ,蛋白質(g): 0",
-    "燕麥":"碳水化合物(g): 76 ,蛋白質(g): 0",
-    }
-    var input = msg[1:len(msg)]
-    if f, ok := foods[input]; ok {
-    response= "每100克 "+input+" 含: "+f
-    }else{
-    response="找不到: "+input
-    }
+    // for k := range foods {
+    //     if strings.ContainsAny(k,input){
+    //     other=other+k+","
+    //     }
 
-    for k := range foods {
-        if strings.ContainsAny(k,input){
-        other=other+k+","
+    // }
+
+    // return response+"\n您可搜尋類似食物:"+other
+
+    file, err := os.Open("food.csv")
+    if err != nil {
+        fmt.Println("Error:", err)
+        //return
+    }
+    defer file.Close()
+    reader := csv.NewReader(file)
+    reader.Comma = ';'
+    lineCount := 0
+    search:=""
+    for {
+        record, err := reader.Read()
+        if err == io.EOF {
+            break
+        } else if err != nil {
+            fmt.Println("Error:", err)
+            //return
         }
-
+        for i := 0; i < len(record); i++ {
+            if strings.ContainsAny(record[0],input){
+                search=search+record[0][0:strings.Index(record[0], ",")]+","
+            }
+            
+        }
+        lineCount += 1
     }
-
-    return response+"\n您可搜尋類似食物:"+other
+    return response+"\n您可搜尋類似食物:"+search
 }
 
 func rm(msg string) string{
@@ -100,7 +132,7 @@ func rm(msg string) string{
 
 
 func bodyinfo(msg string) string{
-    response:="輸入資料有誤,格式為: b-身高(公分)-體重(公斤)-年齡(整數)-性別(1男0女),ex: b180-70-25-1"
+    response:="輸入資料有誤,格式為: b身高(公分)-體重(公斤)-年齡(整數)-性別(1男0女),ex: b180-70-25-1"
     if strings.ContainsAny(msg,"-"){
         data:=strings.Split(msg[1:len(msg)],"-")
 

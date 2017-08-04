adae315037b02b4fe9ad58a121b300903d0083e5
ray5hen <limboray1@gmail.com>
2017-08-04 11:36:13 +0800

    update food function

--- tools/gymtool.go
+++ tools/gymtool.go
@@ -56,70 +56,93 @@ func weight(code string, msg string) string{
 }
 
 func food(msg string) string{
-    ///response :="No this food." 
-     //var response string
-    // other:=""
-     response:="找不到您輸入的食物,"
-    // foods := map[string]string{
-    // "雞蛋":"碳水化合物(g): 0 ,蛋白質(g): 6",
-    // "鮭魚":"碳水化合物(g): 0 ,蛋白質(g): 20",
-    // "雞胸":"碳水化合物(g): 0 ,蛋白質(g): 22",
-    // "牛奶":"碳水化合物(g): 4 ,蛋白質(g): 3",
-    // "鯛魚":"碳水化合物(g): 2.5 ,蛋白質(g): 18",
-    // "地瓜":"碳水化合物(g): 25 ,蛋白質(g): 1.3",
-    // "藜麥":"碳水化合物(g): 68 ,蛋白質(g): 12",
-    // "奇亞籽":"碳水化合物(g): 44 ,蛋白質(g): 16",
-    // "糙米":"碳水化合物(g): 73 ,蛋白質(g): 9",
-    // "鮪魚罐頭":"碳水化合物(g):  ,蛋白質(g): 30",
-    // "藍莓":"碳水化合物(g): 13 ,蛋白質(g): 0",
-    // "燕麥":"碳水化合物(g): 76 ,蛋白質(g): 0",
-    // }
-     var input = msg[1:len(msg)]
-    // if f, ok := foods[input]; ok {
-    // response= "每100克 "+input+" 含: "+f
-    // }else{
-    // response="找不到: "+input
-    // }
+    foodIng := map[string]string{
+        "k": "熱量",
+        "p": "蛋白質",
+        "f": "脂肪",
+        "c": "碳水化合物",
+    }
 
-    // for k := range foods {
-    //     if strings.ContainsAny(k,input){
-    //     other=other+k+","
-    //     }
-
-    // }
-
-    // return response+"\n您可搜尋類似食物:"+other
-
-    file, err := os.Open("foods.csv")
+    file, err := os.Open("foods3.csv")
     if err != nil {
         fmt.Println("Error:", err)
-        //return
     }
+    input := msg
     defer file.Close()
     reader := csv.NewReader(file)
     reader.Comma = ';'
     lineCount := 0
-    search:=""
+    search := ""
+    response := ""
     for {
         record, err := reader.Read()
         if err == io.EOF {
             break
         } else if err != nil {
             fmt.Println("Error:", err)
-            //return
         }
         for i := 0; i < len(record); i++ {
-            if record[0][0:strings.Index(record[0], ",")]==input{
-                food:=strings.Split(record[0],",")
-                response=fmt.Sprintf("%s 含有"+" 熱量(Kcal)%s"+" 蛋白質(g)%s"+" 脂肪(g):%s"+" 碳水化合物(g):%s",food[0],food[1],food[2],food[3],food[4])
-            }
-            if strings.ContainsAny(record[0],input){
-                search=search+record[0][0:strings.Index(record[0], ",")]+","
+
+            if strings.ContainsAny(input, "-") {
+                data := strings.Split(input[1:len(input)], "-")
+                food := strings.Split(record[0], ",")
+                criteria, err := strconv.ParseFloat(strings.TrimSpace(data[1]), 64)
+                foddKcal, err := strconv.ParseFloat(food[1], 64)
+                foddProtein, err := strconv.ParseFloat(food[2], 64)
+                foddFat, err := strconv.ParseFloat(food[3], 64)
+                foddCarbon, err := strconv.ParseFloat(food[4], 64)
+
+                if err == nil {
+                    switch data[0] {
+                    case string('k'):
+                        if foddKcal >= criteria {
+                            response = response + fmt.Sprintf("%s(%s)", food[0], food[1])
+                        }
+
+                    case string('p'):
+                        if foddProtein >= criteria {
+                            response = response + fmt.Sprintf("%s(%s) ", food[0], food[2])
+                        }
+
+                    case string('f'):
+                        if foddFat >= criteria {
+                            response = response + fmt.Sprintf("%s(%s) ", food[0], food[3])
+                        }
+
+                    case string('c'):
+                        if foddCarbon >= criteria {
+                            response = response + fmt.Sprintf("%s(%s) ", food[0], food[4])
+                        }
+
+                    }
+                }else{
+                    response="輸入錯誤或無法辨識"
+                }
+            } else {
+
+                if record[0][0:strings.Index(record[0], ",")] == input {
+                    food := strings.Split(record[0], ",")
+                    response = fmt.Sprintf("%s 含有"+" 熱量(Kcal)%s"+" 蛋白質(g)%s"+" 脂肪(g):%s"+" 碳水化合物(g):%s", food[0], food[1], food[2], food[3], food[4])
+                }
+                if strings.ContainsAny(record[0], "雞") {
+                    search = search + record[0][0:strings.Index(record[0], ",")] + ","
+                }
+
             }
         }
         lineCount += 1
     }
-    return response+"\n\n您可搜尋類似的食物:\n"+ search
+
+    if strings.ContainsAny(input, "-") {
+    response = fmt.Sprintf( foodIng[strings.ToLower(string(input[1]))]+"大於"+strings.TrimSpace(input[3:len(input)])+"的食物有:" +response)
+    }else{
+    if response=="" {
+    response="找不到您輸入的食物"
+    }
+    response=fmt.Sprintf(response + "\n\n您可搜尋類似的食物:\n" + search[0:len(search)-1])
+    }
+
+    return response
 }
 
 func rm(msg string) string{
@@ -210,7 +233,7 @@ response:="目前提供下列運動課表\n1. 5-3-1 訓練: s-fto-部位(s,b,d)-
 
 data:=strings.Split(msg[1:len(msg)],"-")
 
-if data[1]=="fto"{
+if strings.ToLower(data[1])=="fto"{
     day ,err:=strconv.Atoi(data[3])
     weight ,err:=strconv.ParseFloat(data[4],64)
     if err==nil && day <5{
@@ -218,9 +241,9 @@ if data[1]=="fto"{
     case 1:
         response=fmt.Sprintf("%s-第%s週(每組5Reps)\n第一組%.2f\n第二組%.2f\n第三組%.2f\n第J+1組%.2f\n第J+2組%.2f\n第J+3組%.2f\n",workoutMap[data[2]],data[3],ftoMap[1][1]*weight,ftoMap[1][2]*weight,ftoMap[1][3]*weight,ftoMap[1][4]*weight*ftoMap[1][3],ftoMap[1][5]*weight*ftoMap[1][3],ftoMap[1][6]*weight*ftoMap[1][3])   
     case 2:
-        response=fmt.Sprintf("%s-第%s週(每組3Reps)\n第一組%.2f\n第二組%.2f\n第三組%.2f\n第J+1組%.2f\n第J+2組%.2f\n第J+3組%.2f\n",workoutMap[data[2]],data[3],ftoMap[2][1]*weight,ftoMap[2][2]*weight,ftoMap[2][3]*weight,ftoMap[2][4]*weight*ftoMap[2][3],ftoMap[2][5]*weight*ftoMap[2][3],ftoMap[2][6]*weight*ftoMap[2][3])   
+        response=fmt.Sprintf("%s-第%s週(每組3Reps)\n第一組%.2f\n第二組%.2f\n第三組%.2f\n第J+1組%.2f(破PR組)\n第J+2組%.2f(破PR組)\n第J+3組%.2f(破PR組)\n",workoutMap[data[2]],data[3],ftoMap[2][1]*weight,ftoMap[2][2]*weight,ftoMap[2][3]*weight,ftoMap[2][4]*weight*ftoMap[2][3],ftoMap[2][5]*weight*ftoMap[2][3],ftoMap[2][6]*weight*ftoMap[2][3])   
     case 3:
-        response=fmt.Sprintf("%s-第%s週(5,3,1,1,1 Reps)\n第一組%.2f\n第二組%.2f\n第三組%.2f\n第J+1組%.2f\n第J+2組%.2f\n第J+3組%.2f\n",workoutMap[data[2]],data[3],ftoMap[3][1]*weight,ftoMap[3][2]*weight,ftoMap[3][3]*weight,ftoMap[3][4]*weight*ftoMap[3][3],ftoMap[3][5]*weight*ftoMap[3][3],ftoMap[3][6]*weight*ftoMap[3][3])    
+        response=fmt.Sprintf("%s-第%s週(5,3,1,1,1 Reps)\n第一組%.2f\n第二組%.2f\n第三組%.2f\n第J+1組%.2f\n第J+2組%.2f(破PR組)\n第J+3組%.2f(破PR組)\n",workoutMap[data[2]],data[3],ftoMap[3][1]*weight,ftoMap[3][2]*weight,ftoMap[3][3]*weight,ftoMap[3][4]*weight*ftoMap[3][3],ftoMap[3][5]*weight*ftoMap[3][3],ftoMap[3][6]*weight*ftoMap[3][3])    
     case 4:
         response=fmt.Sprintf("%s-第%s週(每組5Reps)\n第一組%.2f\n第二組%.2f\n第三組%.2f",workoutMap[data[2]],data[3],ftoMap[4][1]*weight,ftoMap[4][2]*weight,ftoMap[4][3]*weight) 
     }
